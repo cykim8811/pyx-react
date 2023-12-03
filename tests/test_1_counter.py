1b2110a4646afd32dfa91e6cdb6445a30c6271a0
Kim Changyeon <cykim8811@snu.ac.kr>
2023-12-03 07:24:03 +0000

    Fix test case for counter

--- tests/test_1_counter.py
+++ tests/test_1_counter.py
@@ -5,15 +5,16 @@ class Counter:
     def __init__(self):
         self.count = 0
 
-    async def increment(self, event):
-        print(await event.button)
-        self.count += 1
-        return
+    def increment(self, event):
+        if await event.altKey:
+            self.count -= 1
+        else:
+            self.count += 1
 
     def __render__(self, user):
         return createElement('div', {},
-            createElement('button', {'onClick': self.increment}, 'Increment'),
-            createElement('p', {}, f'Count: {self.count}')
+            createElement('p', {}, f'Count: {self.count}'),
+            createElement('button', {'onClick': self.increment}, 'Increment')
         )
 
 app = App(Counter())
