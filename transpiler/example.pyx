425ec86c68987a4a8572d8c13b19b72da2d3a23c
cykim <cykim8811@snu.ac.kr>
2023-12-27 16:40:17 +0000

    Fix remaining bugs with pyx transpiler

--- transpiler/example.pyx
+++ transpiler/example.pyx
@@ -1,7 +1,32 @@
-(<div>sdf
-ab
-   c
-   bca
-   <p>abc</p>
-</div>)
+
+import pyx
+
+class Counter:
+   def __init__(self):
+      self.count = 0
+   
+   def __render__(self, user):
+      async def increment(e):
+         print(">", await e.button)
+         if (await e.button) == 0:
+            self.count += 1
+         else:
+            self.count -= 1
+      
+      def contextMenu(e):
+         pass
+      contextMenu.preventDefault = True
+      
+      return (
+         <div>
+            <p>Count: <span style={{'color': 'blue', 'font-weight': 'bold'}}>{self.count}</span></p>
+            <button
+               onMouseDown={increment}
+               onContextMenu={contextMenu}
+               >+</button>
+         </div>
+      )
+
+app = pyx.App(Counter())
+app.run()
 
